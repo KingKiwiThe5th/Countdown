a57a557dd173299d4d19eeaa991a217b24c1d4d9
KingKiwiThe4th <ohalitay@gmail.com>
2026-07-25 21:12:32 +0300

    bad buttons

--- Scripts/countdown_button.gd
+++ Scripts/countdown_button.gd
@@ -1,12 +1,21 @@
 extends Node2D
 
+@export_enum("good","bad") var type = "good"
 @export var countdown_time :int = 20
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+@onready var bad_label: Label = $Bad_label
 
 
 var used = false
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player" and !used:
 		used = true
-		anim.play("activated")
-		Countdown.countdown_reset(countdown_time)
+		if type == "good":
+			anim.play("activated")
+			Countdown.countdown_reset(countdown_time)
+		else:
+			anim.play("activated_bad")
+			Countdown.countdown_time -= 5
+			bad_label.visible = true
+			var tween = create_tween()
+			tween.tween_property(bad_label,"modulate:a",0.0,1.0)
