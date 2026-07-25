65b74070fca8f9fe9c880027a72d5ef278bd3a48
KingKiwiThe4th <ohalitay@gmail.com>
2026-07-26 02:09:05 +0300

    working game over menu and start button

--- Scripts/countdown.gd
+++ Scripts/countdown.gd
@@ -1,7 +1,12 @@
 extends Node
 
+const DEFAULT_TIME = 30
+
 var countdown_time : int = 30
 var lifts: Array = []
-
 func countdown_reset(new_time):
 	countdown_time = new_time
+
+func reset():
+	lifts.clear()
+	countdown_time = DEFAULT_TIME
---
+++ Scripts/game_over_screen.gd
@@ -0,0 +1,8 @@
+extends CanvasLayer
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/control.tscn")
+
+func _on_play_again_pressed() -> void:
+	get_tree().reload_current_scene()
+	Countdown.reset()
