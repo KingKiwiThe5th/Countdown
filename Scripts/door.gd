251bd54996f0811df54426ede2da3685ca48dd6c
KingKiwiThe4th <ohalitay@gmail.com>
2026-07-26 16:03:15 +0300

    room gearbox and shi

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -2,9 +2,11 @@ extends Node2D
 
 @export var door_id: int
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
 func _ready() -> void:
 	Countdown.lifts.append(self)
 
 func unlock():
 	sprite.visible = false
+	collision_shape.disabled = true
